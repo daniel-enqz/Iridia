7d1edc02ec2dfe6496ce7c97e33310c60e498038
Daniel Enriquez <daniel.enm17@gmail.com>
2022-05-02 17:04:17 -0500

    migrations created

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_05_02_205505) do
+ActiveRecord::Schema[7.0].define(version: 2022_05_02_215922) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "donations", force: :cascade do |t|
+    t.string "name"
+    t.text "descrption"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
