6ae9e67d298c35c3d271c2c4e636d9f6805cf4b1
Daniel Enriquez <daniel.enm17@gmail.com>
2022-05-02 18:22:29 -0500

    status field added to donations.

--- app/models/donation.rb
+++ app/models/donation.rb
@@ -2,4 +2,6 @@ class Donation < ApplicationRecord
 
   belongs_to :owner, class_name: "User"
   belongs_to :customer, class_name: "User"
+
+  enum status: { unclaimed: 0, claimed: 10 }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_05_02_224113) do
+ActiveRecord::Schema[7.0].define(version: 2022_05_02_232048) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -21,6 +21,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_05_02_224113) do
     t.datetime "updated_at", null: false
     t.bigint "owner_id", null: false
     t.bigint "customer_id"
+    t.integer "status", default: 0, null: false
     t.index ["customer_id"], name: "index_donations_on_customer_id"
     t.index ["owner_id"], name: "index_donations_on_owner_id"
   end
