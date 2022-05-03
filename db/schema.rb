282b18d8948d9725a76c0ae77577f9f5e1e132f2
Daniel Enriquez <daniel.enm17@gmail.com>
2022-05-03 10:22:43 -0500

    donation column description fixed

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_05_02_234943) do
+ActiveRecord::Schema[7.0].define(version: 2022_05_03_152100) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "donations", force: :cascade do |t|
     t.string "name"
-    t.text "descrption"
+    t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "owner_id", null: false
