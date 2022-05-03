d5e8148508d0986d068595f23e400ed2d3881e7e
Daniel Enriquez <daniel.enm17@gmail.com>
2022-05-02 19:54:13 -0500

    donation policy added to index

--- app/controllers/donations_controller.rb
+++ app/controllers/donations_controller.rb
@@ -1,5 +1,7 @@
 class DonationsController < ApplicationController
   skip_before_action :authenticate_user!, only: :index
 
-  def index; end
+  def index
+    @donations = policy_scope(Donation).order(created_at: :desc)
+  end
 end
--- app/policies/donation_policy.rb
+++ app/policies/donation_policy.rb
@@ -1,8 +1,10 @@
 class DonationPolicy < ApplicationPolicy
   class Scope < Scope
     # NOTE: Be explicit about which records you allow access to!
-    # def resolve
-    #   scope.all
-    # end
+    def resolve
+      scope.all
+    end
+
+
   end
 end
