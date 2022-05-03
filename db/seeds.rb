3799740dbb20388041e64f21cf8f67bfdfa9857f
Daniel Enriquez <daniel.enm17@gmail.com>
2022-05-03 18:12:54 -0500

    seeds created

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,10 +5,29 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
-default_password = "123456"
-require "open-uri"
 
+# USERS POPULATION
+default_password = "123456"
+print "Populating Users"
 5.times do |n|
+  print "."
   User.create!(email: "user#{n}@user.com", password: default_password,
                           first_name: Faker::Name.first_name, last_name: Faker::Name.last_name)
 end
+puts "."
+puts "✅ 5 Users were created successfully"
+
+# DONATIONS POPULATION
+categories = ["Food", "Clothing", "Electronics", "Other"]
+print "Creating Donations❤️"
+20.times do |n|
+  print "."
+  user = User.all.sample
+  Donation.create!(name: Faker::Commerce.product_name, owner: user, customer: user, category: categories.sample,
+  description: Faker::Lorem.sentence(word_count: 10, supplemental: true) )
+end
+puts "."
+puts "✅ 20 Donations were created successfully"
+
+puts "For logging in you can either create an account or use...."
+puts "user1@user.com | password: 123456"
