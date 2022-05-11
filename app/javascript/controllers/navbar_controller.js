a1f955e0a59ff9ed833b37774f91a3723e6fff0b
Daniel Enriquez <daniel.enm17@gmail.com>
2022-05-11 18:59:55 -0500

    navbar added

--- app/javascript/controllers/navbar_controller.js
+++ app/javascript/controllers/navbar_controller.js
@@ -3,11 +3,11 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   updateNavbar() {
     if (window.scrollY) {
-      this.element.classList.remove("navbar-transparent")
-      this.element.classList.add("navbar-white")
-    } else {
-      this.element.classList.remove("navbar-white")
       this.element.classList.add("navbar-transparent")
+      this.element.classList.remove("navbar-white")
+    } else {
+      this.element.classList.add("navbar-white")
+      this.element.classList.remove("navbar-transparent")
     }
   };
 }
