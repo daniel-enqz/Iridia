36340a4101406ce140a21974c4a123ca02192e42
Daniel Enriquez <daniel.enm17@gmail.com>
2022-06-07 16:58:15 -0500

    sign in form advancing

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -8,19 +8,38 @@ module.exports = {
   ],
   theme: {
     colors: {
-      transparent: 'transparent',
-      current: 'currentColor',
-      'white': '#ffffff',
-      'background': '#F3F4F6',
-      'purple': '#3f3cbb',
-      'midnight': '#121063',
-      'light-midnight': '#60A5FA',
-      'metal': '#565584',
-      'tahiti': '#3ab7bf',
-      'silver': '#ecebff',
-      'bubble-gum': '#ff77e9',
-      'bermuda': '#78dcca',
-      'black': '#1C1917',
+      // Primary
+      'primary': '#8378DF',
+      'light_purple': '#E8E6F6',
+
+      // Secondary
+      'seafoam': '#80D3AA',
+      'electric_teal': '#E9FFF4',
+      'pink-elephant': '#EF9DE0',
+      'light_pink': '#FFF0FC',
+
+      // States
+      'fountain_green': '#37CCB2',
+      'red_punch': '#EA343E',
+      'yellow_orange': '#FEBB5A',
+      'grey': '#EFF2F7',
+
+      // Text
+      'second_grey': '#1F2D3D',
+      'white': '#FFFFFF',
+
+      // Shades of greys
+      'three_grey':'#3C4858',
+      'four_grey':'#8492A6',
+      'five_grey':'#C2CEDB',
+      'six_grey':'#D3DCE6',
+      'seven_grey':'#E5E9F2',
+      'eigth':'#EFF2F7',
+      'nine':'#F9FAFC',
+      // Extra
+      'medium_blue': '#0782D0',
+      'dark_blue': '#113C7B',
+      'salmon':'#FC8383',
     },
     extend: {
       fontFamily: {
